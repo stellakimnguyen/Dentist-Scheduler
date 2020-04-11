579aec6a9427837ec52906048ffd1892f9284249
Stella Nguyen <stellakimnguyen@gmail.com>
2020-04-10 21:28:48 -0400

    Change to php for html import

---
+++ UserInput.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+
+  <title>Dental Clinics</title>
+  <meta name="main" content="all">
+
+  <link rel="stylesheet" href="UserInput.css">
+  <script src="UserInput.js"></script>
+</head>
+
+<body>
+    <div class="main-container">
+      <?php include 'RequestInput.html';?>
+      <?php include 'DBModifications.html';?>
+    </div>
+    
+</body>
+
+</html>
